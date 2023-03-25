e2202f8946b6cafb5131293786d7b528fc7f8be7
John Whitten <johnwhitten.studio@gmail.com>
2023-03-25 14:04:08 -0700

    adjust weather and map api key

--- src/weather-service.js
+++ src/weather-service.js
@@ -1,6 +1,6 @@
 export default class WeatherService {
   static getWeatherForecast(location) {
-    return fetch(`http://api.openweathermap.org/data/2.5/forecast?appid=${process.env.API_KEY_WEATHER}&q=${location}&units=imperial`)
+    return fetch(`http://api.openweathermap.org/data/2.5/forecast?appid=f07e1afd7c31671932a1d348b94a5696&q=${location}&units=imperial`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
