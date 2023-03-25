147d48b3ff27f57760d043aad36c0db43d3ecb34
John Whitten <johnwhitten.studio@gmail.com>
2023-03-25 15:12:40 -0700

    adjust weather and map api key

--- src/map-service.js
+++ src/map-service.js
@@ -1,12 +1,12 @@
 export default class MapService {
   static getMap(lat,lon) {
 
-    return fetch(`https://maps.locationiq.com/v3/staticmap?key=pk.4ce97d411fc5a10dda9888272f18f372&center=${lat},${lon}&zoom=10`)
+    return fetch(`https://maps.locationiq.com/v3/staticmap?key=${process.env.API_KEY_MAP}&center=${lat},${lon}&zoom=10`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
         }
-        return `https://maps.locationiq.com/v3/staticmap?key=pk.4ce97d411fc5a10dda9888272f18f372&center=${lat},${lon}&zoom=10`;
+        return `https://maps.locationiq.com/v3/staticmap?key=${process.env.API_KEY_MAP}&center=${lat},${lon}&zoom=10`;
       })
       .catch(function(error) {
         return Error(error);
